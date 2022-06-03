5b721f843bd9a741707044ddf191335aa8a141c8
Raphael Fontoura <raphael.fontoura@gmail.com>
2022-06-03 09:08:47 -0300

    Add log info to fraud controller

--- fraud/src/main/java/com/rdeveloper/fraud/FraudController.java
+++ fraud/src/main/java/com/rdeveloper/fraud/FraudController.java
@@ -1,11 +1,13 @@
 package com.rdeveloper.fraud;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/fraud-check")
 @AllArgsConstructor
+@Slf4j
 public class FraudController {
 
     private FraudCheckService fraudCheckService;
@@ -13,6 +15,7 @@ public class FraudController {
     @GetMapping(path = "{customerId}")
     public FraudCheckResponse isFrauder(@PathVariable("customerId") Integer customerId) {
         boolean fraudulentCustomer = fraudCheckService.isFraudulentCustomer(customerId);
+        log.info("fraud check request for customer {}", customerId);
         return new FraudCheckResponse(fraudulentCustomer);
     }
 }
