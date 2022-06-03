4d6984580cba8fb8316e6beb2b551dbee0bf3b2a
Raphael Fontoura <raphael.fontoura@gmail.com>
2022-06-03 16:10:12 -0300

    Modify customer service to use fraud service by eureka-server

--- customer/src/main/java/com/rdeveloper/customer/CustomerService.java
+++ customer/src/main/java/com/rdeveloper/customer/CustomerService.java
@@ -16,7 +16,7 @@ public record CustomerService(CustomerRepository customerRepository, RestTemplat
         // todo: check if email not taken
         customerRepository.saveAndFlush(customer);
         FraudCheckResponse fraudCheckResponse = restTemplate.getForObject(
-                "http://localhost:8081/api/v1/fraud-check/{customerId}",
+                "http://FRAUD/api/v1/fraud-check/{customerId}",
                 FraudCheckResponse.class,
                 customer.getId()
         );
