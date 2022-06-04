f4d3edb371b26916dc281e1ad5ea8ca8de4b2312
Raphael Fontoura <raphael.fontoura@gmail.com>
2022-06-03 22:02:59 -0300

    Add notification microservice module. Config docker-compose with rabbitmq

---
+++ notification/src/main/java/com/rdeveloper/notification/NotificationRepository.java
@@ -0,0 +1,6 @@
+package com.rdeveloper.notification;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NotificationRepository extends JpaRepository<Notification, Integer> {
+}
